c044844af298cb36597a014168caf39d77800ab9
Nathan Marz <nathan@redplanetlabs.com>
2023-11-24 09:25:58 -1000

    copy release as rama.zip

--- rama-cluster/upload_rama.sh
+++ rama-cluster/upload_rama.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 # Upload the rama.zip file to the /data/rama directory on the conductor
-scp -o "StrictHostKeyChecking no" "$1" "$2@$3:/home/$2"
+scp -o "StrictHostKeyChecking no" "$1" "$2@$3:/home/$2/rama.zip"
