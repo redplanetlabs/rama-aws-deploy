93b93fe0df64f1eba7accfb4ba39ecd65ed4177f
Philip Dumaresq <phdumaresq@protonmail.com>
2023-12-08 16:54:08 -0500

    Remove previous curl command

--- rama-cluster/download_rama.sh
+++ rama-cluster/download_rama.sh
@@ -2,11 +2,6 @@
 
 cd /data/rama
 
-# TODO: get this from the conductor via scp 
-
-curl --connect-timeout 180 'http://${conductor_ip}:8888/d/jar/download/rama.zip' \
-     --output rama.zip
-
 until curl -s -f 'http://${conductor_ip}:8888/d/jar/download/rama.zip' \
            --output rama.zip
 do
