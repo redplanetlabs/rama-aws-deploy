4174049f6769c236c600769962ffc5ce39f888e9
AJ LaMarc <ajlamarc@gmail.com>
2023-12-13 10:24:24 -0500

    use IMDSv2 instance metadata curl method (#2)

--- rama-cluster/supervisor/start.sh
+++ rama-cluster/supervisor/start.sh
@@ -2,14 +2,15 @@
 
 mv /run/rama/rama.yaml /data/rama/rama.yaml
 
-PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 
 cat <<EOF >> /data/rama/rama.yaml
 
 supervisor.host:
-  external: $PUBLIC_IP
-  internal: $PRIVATE_IP
+  external: "$PUBLIC_IP"
+  internal: "$PRIVATE_IP"
 EOF
 
 systemctl enable supervisor.service
