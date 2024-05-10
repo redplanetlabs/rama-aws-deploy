24c83ae1bfea8dc59883a54fa9cecdf26d3d5445
Sunil KS <kslvsunil@gmail.com>
2024-05-10 22:35:58 +0530

    Configure cluster using only internal hostnames (#7)

--- rama-cluster/supervisor/start.sh
+++ rama-cluster/supervisor/start.sh
@@ -4,13 +4,10 @@ mv /run/rama/rama.yaml /data/rama/rama.yaml
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
-PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 
 cat <<EOF >> /data/rama/rama.yaml
 
-supervisor.host:
-  external: "$PUBLIC_IP"
-  internal: "$PRIVATE_IP"
+supervisor.host: "$PRIVATE_IP"
 EOF
 
 systemctl enable supervisor.service
